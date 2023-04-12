a63e9125d4ac968f12b5dc737595d5d5049e2f0c
Any97Cris <criscianysilva1997@gmail.com>
2023-04-12 16:42:48 -0400

    usando os componentes

--- resources/views/tarefas/criar.blade.php
+++ resources/views/tarefas/criar.blade.php
@@ -10,24 +10,20 @@
         <form action="">
             {{-- Título --}}
             <x-form.text_input name="titulo" label="Título da Tarefa" requiered="requiered" placeholder="Digite aqui o título da tarefa" />
+            
             {{-- Data --}}
             <x-form.text_input name="due_data" label="Data da Tarefa" requiered="requiered" type="date"/>
+            
             {{-- Categoria --}}
-            <x-form>
-            
-            
-            <div class="inputArea">
-                <label for="descricao">
-                    Descrição
-                </label>
-                <textarea class="inputArea" name="" id="" cols="30" rows="10" placeholder="Digite a descrição da tarefa"></textarea>
-            </div>
+            <x-form.select_input name="categoria" label="Categoria" equiered="requiered">
+                <option>Valor qualquer</option>
+            </x-form.select_input>
 
-            <div class="inputArea">
-                <button type="submit" class="btn btn-primary">
-                    Criar
-                </button>
-            </div>
+            {{-- Descrição Tarefa --}}
+            <x-form.textarea_input name="descricao" label="Descrição" equiered="requiered" placeholder="Gigite a descrição da tarefa" />
+
+            <x-form.btn_form submitTxt="Criar Tarefa" resetTxt="Limpar Campo"/>
+            
         </form>
     </section>
    
