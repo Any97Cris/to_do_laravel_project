54e8d1815ff38947de1fc066856efd2920270c31
Any97Cris <criscianysilva1997@gmail.com>
2023-04-12 16:42:39 -0400

    textarea

---
+++ resources/views/components/form/textarea_input.blade.php
@@ -0,0 +1,10 @@
+<div class="inputArea">
+    <label for="{{$name}}">
+        {{$label ?? ''}}
+    </label>
+    <textarea id="{{$name}}" name="{{$name}}" 
+    type="{{empty($type) ? 'text' : $type}}" 
+    placeholder="{{$placeholder ?? ''}}" 
+    requiered = "{{empty($requiered) ? 'requiered' : ''}}"></textarea>
+    
+</div>
